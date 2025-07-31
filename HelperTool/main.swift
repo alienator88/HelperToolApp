0784d2b2c34b20188f3c6c26fa97d7720e91d758
Alin Lupascu <lupascu.marius@gmail.com>
2025-07-31 09:58:07 -0600

    Add codesigning validation to the helper

--- HelperTool/main.swift
+++ HelperTool/main.swift
@@ -16,6 +16,12 @@ public protocol HelperToolProtocol {
 class HelperToolDelegate: NSObject, NSXPCListenerDelegate, HelperToolProtocol {
     // Accept new XPC connections by setting up the exported interface and object.
     func listener(_ listener: NSXPCListener, shouldAcceptNewConnection newConnection: NSXPCConnection) -> Bool {
+        // Validate that the main app and helper app have the same code signing identity, otherwise return
+        guard isValidClient(connection: newConnection) else {
+            print("❌ Rejected connection from unauthorized client")
+            return false
+        }
+
         newConnection.exportedInterface = NSXPCInterface(with: HelperToolProtocol.self)
         newConnection.exportedObject = self
         newConnection.resume()
@@ -40,6 +46,16 @@ class HelperToolDelegate: NSObject, NSXPCListenerDelegate, HelperToolProtocol {
         let data = pipe.fileHandleForReading.readDataToEndOfFile()
         let output = String(data: data, encoding: .utf8)?.trimmingCharacters(in: .whitespacesAndNewlines) ?? ""
         reply(output.isEmpty ? "No output" : output)
+    }
+
+    // Check that the codesigning matches between the main app and the helper app
+    private func isValidClient(connection: NSXPCConnection) -> Bool {
+        do {
+            return try CodesignCheck.codeSigningMatches(pid: connection.processIdentifier)
+        } catch {
+            print("Helper code signing check failed with error: \(error)")
+            return false
+        }
     }
 }
 
